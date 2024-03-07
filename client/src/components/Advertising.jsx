554651db33eab87886907672f5a83a61057f6f4c
Ashish Mishra <ashishgk1999@gmail.com>
2024-03-07 18:26:45 +0530

    add image of advertising

---
+++ client/src/components/Advertising.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import advertiseImage from "../assets/addvertise.png";
+
+const Advertising = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <img src={advertiseImage} alt="" className="h-60" />
+    </div>
+  );
+};
+
+export default Advertising;
